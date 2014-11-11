61ec3243e1a57b1ac8cc81ccdbd20a46b44e85c6
Chinemelu Ezeh <chinemeluezeh@gmail.com>
2014-11-11 22:34:38 +0000

    Intermediate

--- src/recorder.cpp
+++ src/recorder.cpp
@@ -30,13 +30,6 @@
 #include <iomanip>  // for controlling float print precision
 #include <sstream>  // string to number conversion
 #include <fstream>      // std::ofstream
-#include <pylon/PylonIncludes.h>
-#ifdef PYLON_WIN_BUILD
-#include <pylon/PylonGUI.h>
-#endif
-#include <pylon/PylonImage.h>
-#include <pylon/Pixel.h>
-#include <pylon/ImageFormatConverter.h>
 #include <cv.h>
 #include <opencv2/core/core.hpp>        // Basic OpenCV structures (cv::Mat, Scalar)
 #include <opencv2/imgproc/imgproc.hpp>  // Gaussian Blur
@@ -48,23 +41,17 @@
 
 using namespace cv;
 
-// Namespace for using pylon objects.
-using namespace Pylon;
-
 // Namespace for using cout.
 using namespace std;
 
 // Number of images to be grabbed.
-static const uint32_t c_countOfImagesToGrab = 100;
 char camera_name[100];
 Mat cam_pose;
-CInstantCamera camera;
 VideoCapture capture;
 bool camInitialise = false;
 int width;
 int height;
 int id = 0;
-CGrabResultPtr ptrGrabResult;
 // Opens camera device by id and gets the camera's position.
 void getConfig()
 {
@@ -93,35 +80,35 @@ void getConfig()
 
 void initialiseCam(int deviceNum = 0)
 {
-  // Write a function to open camera
-  // we will use an OpenCV capture
-     capture.open(deviceNum);
-     capture.set(CV_CAP_PROP_FRAME_WIDTH,320);
-     capture.set(CV_CAP_PROP_FRAME_HEIGHT,240);
-     printf("Opening device number : %d", deviceNum);
-     if (!capture.isOpened())
-     {
-       std::cout << "Could not initialize capturing...\n" << std::endl;
-       exit (EXIT_FAILURE);
-     }
-     camInitialise = true;
+  // Write a function to open camera.
+  // we will use an OpenCV capture.
+  capture.open(deviceNum);
+  capture.set(CV_CAP_PROP_FRAME_WIDTH, 320);
+  capture.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
+  printf("Opening device number : %d", deviceNum);
+  if (!capture.isOpened())
+  {
+    std::cout << "Could not initialize capturing...\n" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  camInitialise = true;
 }
 
 void initFrameSize()
 {
-    // Write a function to open camera
-     Mat frame;
-       IplImage *img;
-       capture>>frame;
-       img = cvCreateImage(cvSize(frame.cols,frame.rows), IPL_DEPTH_8U, frame.channels());
-       img->imageData = (char *)frame.data;
-       if (frame.empty())
-       {
-         printf("Failed to open image from capture device");
-         exit(EXIT_FAILURE);
-       }
-       width = img->width;
-       height = img->height;
+  // Write a function to open camera
+  Mat frame;
+  IplImage *img;
+  capture >> frame;
+  img = cvCreateImage(cvSize(frame.cols, frame.rows), IPL_DEPTH_8U, frame.channels());
+  img->imageData = (char *)frame.data;
+  if (frame.empty())
+  {
+    printf("Failed to open image from capture device");
+    exit(EXIT_FAILURE);
+  }
+  width = img->width;
+  height = img->height;
 }
 
 int main(int argc, char* argv[])
@@ -130,61 +117,55 @@ int main(int argc, char* argv[])
   ros::init(argc, argv, "recorder");
 
   ros::NodeHandle n("~");
-
-  ros::Rate loop_rate(10);
+  int fps = 10;
+  ros::Rate loop_rate(fps);
 
   n.getParam("id", id);
 
-  ros::Publisher sema_pub = n.advertise<std_msgs::String>("/global/sema", 1);
 // The exit code of the sample application.
   getConfig();
   initialiseCam();
   initFrameSize();
-// Automagically call PylonInitialize and PylonTerminate to ensure
-// the pylon runtime system is initialized during the lifetime of this object.
-  Pylon::PylonAutoInitTerm autoInitTerm;
 
-  CGrabResultPtr ptrGrabResult;
-
-
+  //Todo: put time into this.
   stringstream sbuffer;
   string videoPath = "/home/ceezeh/catkin_ws/src/more_t2/videos/video_cam_";
-    sbuffer << videoPath << id << ".avi";
+  sbuffer << videoPath << id << ".avi";
 
-    Size frameSize(static_cast<int>(width), static_cast<int>(height));
-    // Todo: Get precise frame rate. This static value is very wrong.
-    Mat temp;
-            capture.read(temp);
-            IplImage* img  = cvCreateImage(cvSize(temp.cols,temp.rows), IPL_DEPTH_8U, temp.channels());
+  Size frameSize(static_cast<int>(width), static_cast<int>(height));
+  // Todo: Get precise frame rate. This static value is very wrong.
+  Mat temp;
+  capture.read(temp);
+  IplImage* img = cvCreateImage(cvSize(temp.cols, temp.rows), IPL_DEPTH_8U, temp.channels());
+  VideoWriter* oVideoWriter = new VideoWriter(sbuffer.str().c_str(), CV_FOURCC('H','2','6','4'), fps,
+                                                    cvSize(width, height), true);
+  oVideoWriter->open(sbuffer.str().c_str(), CV_FOURCC('H','2','6','4'), fps,
+                                                    cvSize(width, height), true);
+  if (!oVideoWriter->isOpened()) //if not initialize the VideoWriter successfully, exit the program
+  {
+    cout << "ERROR: Failed to start video writer" << endl;
+    exit(EXIT_FAILURE);
+  }
 
-  CvVideoWriter* oVideoWriter = cvCreateVideoWriter(sbuffer.str().c_str(), CV_FOURCC_DEFAULT, 5, cvSize(width, height), 0);
-  if ( !oVideoWriter ) //if not initialize the VideoWriter successfully, exit the program
-    {
-         cout << "ERROR: Failed to start video writer" << endl;
-         exit(EXIT_FAILURE);
-    }
-
-  while(ros::ok()) {
+  while (ros::ok())
+  {
     capture.read(temp);
     if (temp.empty())
-            {
-              ROS_INFO("ERROR: files to grab new image\n");
-              exit(EXIT_FAILURE);
-            }
+    {
+      ROS_INFO("ERROR: files to grab new image\n");
+      exit(EXIT_FAILURE);
+    }
 
     img->imageData = (char*)temp.data;
-            cvWriteToAVI(oVideoWriter,img);
-            cvShowImage("Debug", img);
-            cvWaitKey(1); // Wait for image to be rendered on screen. If not included, no image is shown.
+    oVideoWriter->write(img);
+    cvShowImage("Debug", img);
+    cvWaitKey(1); // Wait for image to be rendered on screen. If not included, no image is shown.
 
-          cout << "cam pose" << endl << cam_pose << endl << endl;
+    cout << "cam pose" << endl << cam_pose << endl << endl;
 
-      ros::spinOnce();
-      loop_rate.sleep();
-    }
-  cvReleaseVideoWriter(&oVideoWriter);
-  std_msgs::String msg;
-  msg.data = "Arrived";
-  sema_pub.publish(msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+ oVideoWriter->release();
   return 0;
 }
